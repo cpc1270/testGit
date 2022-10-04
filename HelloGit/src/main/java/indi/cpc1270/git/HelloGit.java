9c08881b49512e167d83ce1b773de57ae0c3830c
cpc1270 <3370562476@qq.com>
2022-10-04 13:42:35 +0800

    添加文件

---
+++ HelloGit/src/main/java/indi/cpc1270/git/HelloGit.java
@@ -0,0 +1,14 @@
+package indi.cpc1270.git;
+
+/**
+ * @author cpc1270
+ * @create 2022-10-04-13:25
+ * description:
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}
